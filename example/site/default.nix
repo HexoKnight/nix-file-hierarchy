a8a17009c8a982acc4b0203cc0a81d24653dfbe9
HexoKnight <harvey.gream@gmail.com>
2024-09-19 01:08:24 +0100

    test: update example to use individual element functions

--- example/site/default.nix
+++ example/site/default.nix
@@ -6,28 +6,30 @@ let
 in
 [
 html.doctype
-(e "html" { lang="en"; } [
-  (e "head" {} [
-    (e "meta" { charset="UTF-8"; } null)
-    (e "meta" { name="viewport"; content="width=device-width, initial-scale=1.0"; } null)
-    (e "title" {} "Test <Page> </title> :)")
-    (e "link" { rel="stylesheet"; href=referenceCSS ./css/style.nix; } null)
-    (e "link" { rel="stylesheet"; href=referenceCSS ./css/style2.css; } null)
-    (e "link" { rel="stylesheet"; href=referenceSCSS ./css/style3.scss; } null)
-    (e "link" { rel="stylesheet"; href=referencePage {
+(e.html { lang="en"; } [
+  (e.head {} [
+    (e.meta { charset="UTF-8"; })
+    (e.meta { name="viewport"; content="width=device-width, initial-scale=1.0"; })
+    (e.title {} "Test <Page> </title> :)")
+    (e.link { rel="stylesheet"; href=referenceCSS ./css/style.nix; })
+    (e.link { rel="stylesheet"; href=referenceCSS ./css/style2.css; })
+    (e.link { rel="stylesheet"; href=referenceSCSS ./css/style3.scss; })
+    (e.link { rel="stylesheet"; href=referencePage {
       content = /* css */ ''
         html {
           background: blue;
         }
       '';
       fullSitePath = "/hmmm/this/random/path/lol.css";
-    }; } null)
-    (e "script" { type="text/javascript"; } (html.mkRaw /* javascript */ ''
+    }; })
+    (e.script { type="text/javascript"; } (/* javascript */ ''
       console.log("<> > &lt;&gt;")
+      // nearly but not quite invalid
+      console.log("</script")
     ''))
   ])
-  (e "body" {} [
-    (e "p" { id="t<e&st"; } "<> > &lt;&gt; : escaping by default")
+  (e.body {} [
+    (e.p { id="t<e&st"; } "<> > &lt;&gt; : escaping by default")
   ])
 ])
 ]
